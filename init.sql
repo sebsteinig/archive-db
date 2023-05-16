d08021ee5e5cd99f159be22fb7122922b79d179c
willemNicolas <darwil.nicolas@gmail.com>
2023-05-16 11:45:57 +0100

    fix

--- init.sql
+++ init.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE archive
-    WITH
-    OWNER = root
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+-- CREATE DATABASE archive
+--    WITH
+--    OWNER = root
+--   ENCODING = 'UTF8'
+--    LC_COLLATE = 'en_US.utf8'
+--    LC_CTYPE = 'en_US.utf8'
+--    TABLESPACE = pg_default
+--    CONNECTION LIMIT = -1
+--    IS_TEMPLATE = False;
 
 CREATE TABLE IF NOT EXISTS public.table_conversion_info
 (
