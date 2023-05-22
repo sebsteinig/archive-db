a3d87e36803d1f5d457fbfc8789f1b55eea51666
akina <renardakina@gmail.com>
2023-05-22 10:49:00 +0100

    new tables

--- init.sql
+++ init.sql
@@ -1,47 +1,75 @@
-CREATE TABLE IF NOT EXISTS public.table_conversion_info
+CREATE TABLE IF NOT EXISTS public.table_experiments
 (
-    pk_id SERIAL PRIMARY KEY,
-    creation_at timestamp with time zone NOT NULL DEFAULT now(),
-    levels bigint NOT NULL DEFAULT 1,
-    time_steps bigint NOT NULL DEFAULT 1,
-    xsize bigint NOT NULL,
-    ysize bigint NOT NULL,
+    exp_id text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    CONSTRAINT table_experiments_pkey PRIMARY KEY (exp_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.table_experiments
+    OWNER to root;
+
+CREATE TABLE IF NOT EXISTS public.table_collections
+(
+    name text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    CONSTRAINT table_collections_pkey PRIMARY KEY (name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.table_collections
+    OWNER to root;
+
+
+CREATE TABLE IF NOT EXISTS public.join_collections_experiments
+(
+    exp_id text COLLATE pg_catalog."default" NOT NULL,
+    collection_name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT collection_name FOREIGN KEY (collection_name)
+        REFERENCES public.table_collections (name) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT exp_id FOREIGN KEY (exp_id)
+        REFERENCES public.table_experiments (exp_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.join_collections_experiments
+    OWNER to root;
+
+
+CREATE TABLE IF NOT EXISTS public.table_variables
+(
+    id serial NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    exp_id text COLLATE pg_catalog."default" NOT NULL,
+    paths text[] COLLATE pg_catalog."default" NOT NULL,
+    created_at date NOT NULL DEFAULT now(),
+    config_name text COLLATE pg_catalog."default" NOT NULL,
+    levels integer NOT NULL,
+    timesteps integer NOT NULL,
+    xsize integer NOT NULL,
     xfirst real NOT NULL,
     xinc real NOT NULL,
+    ysize integer NOT NULL,
     yfirst real NOT NULL,
     yinc real NOT NULL,
-    nan_value_encoding smallint NOT NULL DEFAULT 255,
-    threshold real NOT NULL DEFAULT 3
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.table_conversion_info
-    OWNER to root;
-
-CREATE TABLE IF NOT EXISTS public.table_execution_info
-(
-    pk_id SERIAL PRIMARY KEY,
-    exp_id text COLLATE pg_catalog."default" NOT NULL,
-    config text COLLATE pg_catalog."default" NOT NULL,
-    nimbus_version text COLLATE pg_catalog."default" NOT NULL,
-    execution_date timestamp without time zone NOT NULL DEFAULT now(),
-    var_clt boolean NOT NULL DEFAULT false,
-    var_currents boolean NOT NULL DEFAULT false,
-    var_height boolean NOT NULL DEFAULT false,
-    var_liconc boolean NOT NULL DEFAULT false,
-    var_mlosts boolean NOT NULL DEFAULT false,
-    var_pfts boolean NOT NULL DEFAULT false,
-    var_pr boolean NOT NULL DEFAULT false,
-    var_sic boolean NOT NULL DEFAULT false,
-    var_snc boolean NOT NULL DEFAULT false,
-    var_tas boolean NOT NULL DEFAULT false,
-    var_tos boolean NOT NULL DEFAULT false,
-    var_winds boolean NOT NULL DEFAULT false,
-    metadata json,
-    fk_id_conversion_info integer NOT NULL,
-    CONSTRAINT fk_id_conversion_info FOREIGN KEY (fk_id_conversion_info)
-        REFERENCES public.table_conversion_info (pk_id) MATCH SIMPLE
+    extension text COLLATE pg_catalog."default" NOT NULL,
+    lossless boolean NOT NULL,
+    nan_value_encoding integer NOT NULL,
+    threshold real NOT NULL,
+    chunks integer NOT NULL,
+    metadata json NOT NULL,
+    CONSTRAINT table_variables_pkey PRIMARY KEY (id),
+    CONSTRAINT exp_id FOREIGN KEY (exp_id)
+        REFERENCES public.table_experiments (exp_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
@@ -49,24 +77,5 @@ CREATE TABLE IF NOT EXISTS public.table_execution_info
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.table_execution_info
-    OWNER to root;
-
-CREATE TABLE IF NOT EXISTS public.table_image_paths
-(
-    pk_id BIGSERIAL PRIMARY KEY,
-    fk_id_conversion_info integer NOT NULL,
-    path path NOT NULL,
-    extension text COLLATE pg_catalog."default",
-    metadata json,
-    CONSTRAINT fk_id_conversion_info FOREIGN KEY (pk_id)
-        REFERENCES public.table_conversion_info (pk_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.table_image_paths
+ALTER TABLE IF EXISTS public.table_variables
     OWNER to root;
