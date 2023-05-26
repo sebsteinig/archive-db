920cf0e273113148e9bb24203c0132fe4c93534c
willemNicolas <darwil.nicolas@gmail.com>
2023-05-26 15:18:09 +0100

    add cache for search routes

--- api/routes/search.go
+++ api/routes/search.go
@@ -2,14 +2,20 @@ package routes
 
 import (
 	"archive-api/utils/services"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func BuildSearchRoutes(app *fiber.App, pool *pgxpool.Pool) {
 
 	search_routes := app.Group("/search")
+	search_routes.Use(cache.New(cache.Config{
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
 
 	search_routes.Get("/", func(c *fiber.Ctx) error {
 		return services.SearchExperimentLike(c, pool)
