df2a00ab5131195c183f7f10b4cdcb37ab16ddc6
willemNicolas <darwil.nicolas@gmail.com>
2023-05-26 10:51:44 +0100

    param value

--- api/utils/params_parser.go
+++ api/utils/params_parser.go
@@ -7,39 +7,83 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Params map[string]interface{}
+type ParamValue struct {
+	value    interface{}
+	operator func(string, interface{}, *Placeholder) string
+}
+type Params map[string]ParamValue
 
-func (params Params) ParseParams(c *fiber.Ctx) error {
+func (params Params) ParseParams(c *fiber.Ctx, whitelist ...string) error {
+	default_operator := func(key string, value interface{}, pl *Placeholder) string {
+		return fmt.Sprintf("%s = %s ", key, pl.Get(value))
+	}
 	log.Default().Println(c.Query("config_name"))
-	if value := c.Query("config_name", "##error##"); value != "##error##" {
-		params["config_name"] = value
+	if value := c.Query("config_name", "##error##"); value != "##error##" && in("config_name", whitelist) {
+		params["config_name"] = ParamValue{
+			value:    value,
+			operator: default_operator,
+		}
 	}
-	if value := c.Query("extension", "##error##"); value != "##error##" {
-		params["extension"] = value
+	if value := c.Query("extension", "##error##"); value != "##error##" && in("extension", whitelist) {
+		params["extension"] = ParamValue{
+			value:    value,
+			operator: default_operator,
+		}
 	}
-	if value := c.Query("lossless", "error"); value != "error" {
-		params["lossless"] = c.QueryBool("lossless")
+	if value := c.Query("lossless", "error"); value != "error" && in("lossless", whitelist) {
+		params["lossless"] = ParamValue{
+			value:    c.QueryBool("lossless"),
+			operator: default_operator,
+		}
 	}
-	if value := c.Query("threshold", "error"); value != "error" {
-		params["threshold"] = c.QueryFloat("threshold")
+	if value := c.Query("threshold", "error"); value != "error" && in("threshold", whitelist) {
+		params["threshold"] = ParamValue{
+			value:    c.QueryFloat("threshold"),
+			operator: default_operator,
+		}
 	}
-	if value := c.Query("rx", "error"); value != "error" {
-		params["rx"] = c.QueryFloat("rx")
+	if value := c.Query("rx", "error"); value != "error" && in("rx", whitelist) {
+		params["rx"] = ParamValue{
+			value:    c.QueryFloat("rx"),
+			operator: default_operator,
+		}
 	}
-	if value := c.Query("ry", "error"); value != "error" {
-		params["ry"] = c.QueryFloat("ry")
+	if value := c.Query("ry", "error"); value != "error" && in("ry", whitelist) {
+		params["ry"] = ParamValue{
+			value:    c.QueryFloat("ry"),
+			operator: default_operator,
+		}
 	}
-	if value := c.Query("chunks", "error"); value != "error" {
-		params["chunks"] = c.QueryInt("chunks")
+	if value := c.Query("chunks", "error"); value != "error" && in("chunks", whitelist) {
+		params["chunks"] = ParamValue{
+			value:    c.QueryInt("chunks"),
+			operator: default_operator,
+		}
 	}
-
+	if value := c.Query("id", "##error##"); value != "error" && in("id", whitelist) {
+		params["id"] = ParamValue{
+			value: value,
+			operator: func(key string, value interface{}, pl *Placeholder) string {
+				return fmt.Sprintf("%s LIKE '%s%%' ", key, pl.Get(value))
+			},
+		}
+	}
 	return nil
 }
 
 func (params Params) ParamToSql(pl *Placeholder) string {
 	res := " "
 	for key, value := range params {
-		res += fmt.Sprintf("AND %s = %s ", key, pl.Get(value))
+		res += fmt.Sprintf("AND %s", value.operator(key, value.value, pl))
 	}
 	return res
+}
+
+func in(value string, arr []string) bool {
+	for _, e := range arr {
+		if value == e {
+			return true
+		}
+	}
+	return false
 }
