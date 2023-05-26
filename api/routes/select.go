53a9502128ea0f9163d7f5d6cb50783834193e84
willemNicolas <darwil.nicolas@gmail.com>
2023-05-26 14:34:33 +0100

    cache

--- api/routes/select.go
+++ api/routes/select.go
@@ -2,15 +2,20 @@ package routes
 
 import (
 	"archive-api/utils/services"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func BuildSelectRoutes(app *fiber.App, pool *pgxpool.Pool) {
 
 	select_routes := app.Group("/select")
-
+	select_routes.Use(cache.New(cache.Config{
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
 	select_routes.Get("/:id", func(c *fiber.Ctx) error {
 		id := c.Params("id")
 		return services.GetExperimentByID(id, c, pool)
