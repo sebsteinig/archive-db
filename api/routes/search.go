6edf253e80357b5c37b49eaf97a46cf39aeb624d
willemNicolas <darwil.nicolas@gmail.com>
2023-06-07 16:16:16 +0100

    searching based on publication

--- api/routes/search.go
+++ api/routes/search.go
@@ -38,6 +38,9 @@ func BuildSearchRoutes(app *fiber.App, pool *pgxpool.Pool) {
 	search_routes.Get("/looking", func(c *fiber.Ctx) error {
 		return services.QueryExperiment(c, pool)
 	})
+	search_routes.Get("/publication", func(c *fiber.Ctx) error {
+		return services.SearchExperimentForPublication(c, pool)
+	})
 	search_routes.Get("/", func(c *fiber.Ctx) error {
 		return services.SearchExperimentLike(c, pool)
 	})
