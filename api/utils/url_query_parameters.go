fbf73af667d4058b2ff6e2af845783460167e69c
willemNicolas <darwil.nicolas@gmail.com>
2023-07-06 11:46:05 +0100

    static file

--- api/utils/url_query_parameters.go
+++ api/utils/url_query_parameters.go
@@ -29,23 +29,23 @@ func BuildQueryParameters(c *fiber.Ctx, params_struct any) (QueryParameters, err
 
 	query_parameters := make(QueryParameters)
 
-	keys := make(map[string]string)
-	for j := 0; j < elements.NumField(); j++ {
-		key, ok := elements.Type().Field(j).Tag.Lookup("param")
-		if ok {
-			keys[key] = ""
-		}
-	}
-	correct_query, wrong_key := true, ""
-	c.Context().QueryArgs().VisitAll(func(key, value []byte) {
-		_, ok := keys[string(key)]
-		if !ok {
-			correct_query, wrong_key = false, string(key)
-		}
-	})
-	if !correct_query {
-		return nil, fmt.Errorf("wrong key %s in query parameters", wrong_key)
-	}
+	// keys := make(map[string]string)
+	// for j := 0; j < elements.NumField(); j++ {
+	// 	key, ok := elements.Type().Field(j).Tag.Lookup("param")
+	// 	if ok {
+	// 		keys[key] = ""
+	// 	}
+	// }
+	// correct_query, wrong_key := true, ""
+	// c.Context().QueryArgs().VisitAll(func(key, value []byte) {
+	// 	_, ok := keys[string(key)]
+	// 	if !ok {
+	// 		correct_query, wrong_key = false, string(key)
+	// 	}
+	// })
+	// if !correct_query {
+	// 	return nil, fmt.Errorf("wrong key %s in query parameters", wrong_key)
+	// }
 
 	for i := 0; i < elements.NumField(); i++ {
 		tag := elements.Type().Field(i).Tag
