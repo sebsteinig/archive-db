ded32f11c42527991f4b1ab185cbc5a47ab518e4
willemNicolas <darwil.nicolas@gmail.com>
2023-07-07 17:48:01 +0100

    fix json aggregation

--- api/utils/services/experiment_searching.go
+++ api/utils/services/experiment_searching.go
@@ -270,7 +270,7 @@ func SearchExperimentForPublication(c *fiber.Ctx, pool *pgxpool.Pool) error {
 	}
 	query, err := sql.SQLf(`
 		SELECT 
-			ARRAY_AGG('{"id":"'||join_publication_exp.exp_id || '","metadata":' || join_publication_exp.metadata || '}') as exps,
+			json_agg(json_build_object('id',join_publication_exp.exp_id,'metadata',join_publication_exp.metadata)) as exps,
 			table_publication.title,
 			table_publication.journal,
 			table_publication.owner_name,
@@ -304,16 +304,16 @@ func SearchExperimentForPublication(c *fiber.Ctx, pool *pgxpool.Pool) error {
 		return err
 	}
 	type Response struct {
-		Exps          []string `json:"exps" sql:"exps"`
-		Title         string   `json:"title" sql:"title"`
-		Journal       string   `json:"journal" sql:"journal"`
-		Owner_name    string   `json:"owner_name" sql:"owner_name"`
-		Owner_email   string   `json:"owner_email" sql:"owner_email"`
-		Abstract      string   `json:"abstract" sql:"abstract"`
-		Brief_desc    string   `json:"brief_desc" sql:"brief_desc"`
-		Year          int      `json:"year" sql:"year"`
-		Authors_full  string   `json:"authors_full" sql:"authors_full"`
-		Authors_short string   `json:"authors_short" sql:"authors_short"`
+		Exps          []map[string]any `json:"exps" sql:"exps"`
+		Title         string           `json:"title" sql:"title"`
+		Journal       string           `json:"journal" sql:"journal"`
+		Owner_name    string           `json:"owner_name" sql:"owner_name"`
+		Owner_email   string           `json:"owner_email" sql:"owner_email"`
+		Abstract      string           `json:"abstract" sql:"abstract"`
+		Brief_desc    string           `json:"brief_desc" sql:"brief_desc"`
+		Year          int              `json:"year" sql:"year"`
+		Authors_full  string           `json:"authors_full" sql:"authors_full"`
+		Authors_short string           `json:"authors_short" sql:"authors_short"`
 	}
 	responses, err := sql.Receive[Response](context.Background(), &query, pool)
 	if err != nil {
