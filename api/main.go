02b8241a0ec557e9f6281d56cec282f856570d7c
akina <renardakina@gmail.com>
2023-07-28 15:08:19 +0100

    to release

--- api/main.go
+++ api/main.go
@@ -44,7 +44,7 @@ func main() {
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
-	app.Static("/", "./static")
+
 	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal("Unable to connect to database:", err)
